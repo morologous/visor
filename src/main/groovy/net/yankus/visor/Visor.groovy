889f52340ccc4e3da52f00f9f1342ecbbb5ff926
Jason Yankus <jason@yankus.net>
2012-05-19 09:21:31 -0300

    RIP all semicolons

--- src/main/groovy/net/yankus/visor/Visor.groovy
+++ src/main/groovy/net/yankus/visor/Visor.groovy
@@ -1,21 +1,21 @@
 package net.yankus.visor
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import java.lang.annotation.Target
+import java.lang.annotation.ElementType
 import org.codehaus.groovy.transform.GroovyASTTransformationClass
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @GroovyASTTransformationClass(["net.yankus.visor.VisorASTTransformation"])
 public @interface Visor { 
-    public Class filters() default { match_all { } };
+    public Class filters() default { match_all { } }
     public Class settings() default { 
         node { local = true } 
         http { enabled = false }
     };
-    public String index();
-    public String[] remoteAddresses() default [];
+    public String index()
+    public String[] remoteAddresses() default []
 
 }
