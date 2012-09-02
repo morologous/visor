ac8e70ad1842279dbcefde1cde01b6e48f2505a0
Jason Yankus <jason@yankus.net>
2012-09-01 20:47:54 -0400

    Adding test to demonstrate index success callback behavior

--- src/test/groovy/net/yankus/visor/EngineIndexTest.groovy
+++ src/test/groovy/net/yankus/visor/EngineIndexTest.groovy
@@ -1,12 +1,17 @@
 package net.yankus.visor
 
 
-import org.junit.BeforeClass
-import org.junit.AfterClass
-import org.junit.Test
 import static org.junit.Assert.*
-import groovy.transform.ToString
 import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.Test
 
 class EngineIndexTest {
     
@@ -39,6 +44,28 @@ class EngineIndexTest {
         assertEquals 1, result.count
         assertEquals flurgle, result.list[0]
     }
+	
+	@Test
+	void testIndexCallback() {
+		def callbackCalled = true
+		def blargle = new EngineIndexTestBean(id:''+'blargle'.hashCode(), name:'blargle', num:300) 
+		def indexR = blargle.index()
+		indexR.success = {
+			def result = new EngineIndexTestBean(num:300).search()
+			assertEquals 1, result.count
+			assertEquals blargle, result.list[0]
+			callbackCalled = true
+		}
+		ExecutorService executor = Executors.newFixedThreadPool(1)
+		executor.execute( {
+				while (!callbackCalled) {
+					Thread.sleep(250)
+				}
+				return
+			 } as Runnable)
+		executor.awaitTermination(5, TimeUnit.SECONDS)
+		assertTrue callbackCalled
+	}
 
     @net.yankus.visor.Visor(index='test', settings = { SearchEngineTestHelper.testESSettings.rehydrate(getDelegate(), getOwner(), getThisObject()).call() } )
     @ToString
