9df5dbc344ed6f8f333fc0529938d1fe5da6be8f
Jason Yankus <github@yankus.net>
2017-06-02 16:59:13 -0400

    beginning to adapt to new libs
    First cut of adaptation to the new version of the es libs — still has
    some work to be done related to the settings creation

--- src/main/groovy/net/yankus/visor/TransportClientBuilder.groovy
+++ src/main/groovy/net/yankus/visor/TransportClientBuilder.groovy
@@ -1,13 +1,13 @@
 package net.yankus.visor
 
 import groovy.util.Expando
-import org.elasticsearch.groovy.client.GClient
-import org.elasticsearch.groovy.node.GNode
-import org.elasticsearch.groovy.node.GNodeBuilder
-import static org.elasticsearch.groovy.node.GNodeBuilder.*
+import org.elasticsearch.client.Client
+import org.elasticsearch.node.Node
+import org.elasticsearch.node.NodeBuilder
+import static org.elasticsearch.node.NodeBuilder.*
 import org.elasticsearch.client.transport.TransportClient
 import org.elasticsearch.common.transport.InetSocketTransportAddress
-import org.elasticsearch.common.settings.ImmutableSettings
+import org.elasticsearch.common.settings.Settings
 import groovy.util.logging.Log4j 
 
 @Log4j
@@ -19,17 +19,17 @@ class TransportClientBuilder {
 	def build() {
 		
 		log.info "Creating transportClient for ${remoteAddresses}"
-        def immutableSettings = ImmutableSettings.settingsBuilder()
+        def immutableSettings = Settings.settingsBuilder()
 
         def settingsClosure = settings.rehydrate(immutableSettings, immutableSettings, immutableSettings).call()
 		def datasource = new Expando()
-        datasource.transportClient = new TransportClient(immutableSettings.build())
+        datasource.transportClient =  TransportClient.builder().settings(immutableSettings).build()
         remoteAddresses.each {
         	def (host, port) = it.split(':')
             datasource.transportClient.addTransportAddress(new InetSocketTransportAddress(host, port as int)) 
         }
 
-        datasource.client = new GClient(datasource.transportClient)
+        datasource.client = transportClient //new GClient(datasource.transportClient)
         datasource.close = { client.close() }
 
         datasource
