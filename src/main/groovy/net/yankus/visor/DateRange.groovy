a7bb2528dd74f544e865f2f52e8b6bd01419d30f
Jason Yankus <jason@yankus.net>
2017-06-05 09:30:35 -0400

    Making types serializable.

--- src/main/groovy/net/yankus/visor/DateRange.groovy
+++ src/main/groovy/net/yankus/visor/DateRange.groovy
@@ -1,10 +1,13 @@
 package net.yankus.visor
 
 import groovy.transform.ToString
+import java.io.Serializable
 
 @ToString
-class DateRange {
+class DateRange implements Serializable {
     
+	public static final serialVersionUID = 1L;
+
     def from
     def to
 
--- src/main/groovy/net/yankus/visor/MultiSelect.groovy
+++ src/main/groovy/net/yankus/visor/MultiSelect.groovy
@@ -1,10 +1,13 @@
 package net.yankus.visor
 
 import groovy.transform.ToString
+import java.io.Serializable 
 
-@ToString
-class MultiSelect {
+@ToString 
+class MultiSelect implements Serializable {
     
+	public static final serialVersionUID = 1L;
+
     def values
 
 }
