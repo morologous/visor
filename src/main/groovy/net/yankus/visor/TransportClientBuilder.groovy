0c4f19077781933ea5cc7026c90a5d7ceede7163
Jason Yankus <jason@yankus.net>
2017-06-22 17:39:29 -0400

    modernizing settings closure usage for transport clients.

--- src/main/groovy/net/yankus/visor/TransportClientBuilder.groovy
+++ src/main/groovy/net/yankus/visor/TransportClientBuilder.groovy
@@ -10,20 +10,21 @@ import org.elasticsearch.common.transport.InetSocketTransportAddress
 import org.elasticsearch.common.settings.Settings
 import groovy.util.logging.Log4j 
 
+
 @Log4j
 class TransportClientBuilder {
 	
 	def settings
 	def remoteAddresses
 
-	def build() {
-		
+	def build() {		
 		log.info "Creating transportClient for ${remoteAddresses}"
-        def immutableSettings = Settings.settingsBuilder()
+        def settings = Settings.settingsBuilder()
+		
+		settings(settings)
 
-        def settingsClosure = settings.rehydrate(immutableSettings, immutableSettings, immutableSettings).call()
 		def datasource = new Expando()
-        datasource.transportClient =  TransportClient.builder().settings(immutableSettings).build()
+        datasource.transportClient =  TransportClient.builder().settings(settings).build()
         remoteAddresses.each {
         	def (host, port) = it.split(':')
             datasource.transportClient.addTransportAddress(new InetSocketTransportAddress(host, port as int)) 
